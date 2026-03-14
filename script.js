474de6e941c13de35c6f4c74ca30e9bd4d8a0520
Ann-Altunashvili <altunashvili.anny@gmail.com>
2026-03-15 01:21:52 +0400

    not final changes in some files

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const audio = document.getElementById("music-file");
 const sBtn = document.getElementById("start-btn");
 const pBtn = document.getElementById("playing-btn");
+const footerPlayer = document.querySelector(".footer-player");
 
 //for playing
 sBtn.onclick = function () {
     audio.play();
     sBtn.style.display = "none";
     pBtn.style.display = "flex";
+    footerPlayer.classList.add("is-playing");
 };
 
 // for pause
@@ -14,10 +16,12 @@ pBtn.onclick = function () {
     audio.pause();
     pBtn.style.display = "none";
     sBtn.style.display = "flex";
+    footerPlayer.classList.remove("is-playing");
 };
 
 // // Additional logic: when music ends the button changes
 audio.onended = function() {
     pBtn.style.display = "none";
     sBtn.style.display = "flex";
+    footerPlayer.classList.remove("is-playing");
 };
